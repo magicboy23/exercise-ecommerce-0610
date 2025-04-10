668a9a2cd815030841a895b5400d4d8ee9ec229b
magicboy23 <andreangg04@gmail.com>
2025-04-10 14:12:50 +0700

    feat: add register

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { errorMiddleware } from "./middlewares/error.middleware";
 import cors from "cors";
 import sampleRouter from "./routes/sample.router";
 import { PORT } from "./config/env";
+import authRouter from "./routes/auth.router";
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/samples", sampleRouter);
+app.use("/auth", authRouter);
+
 app.use(errorMiddleware);
 
 app.listen(8000, () => {
