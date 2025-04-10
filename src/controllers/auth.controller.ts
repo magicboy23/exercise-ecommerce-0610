e9da5fdbb1f56ebc5d29a6bcb2e0236bb04899de
magicboy23 <andreangg04@gmail.com>
2025-04-10 15:32:52 +0700

    feat: add login (#2)

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,3 +1,4 @@
+import { loginService } from "../services/auth/login.service";
 import { registerService } from "../services/auth/register.service";
 import { NextFunction, Request, Response } from "express";
 export const registerController = async (
@@ -7,6 +8,18 @@ export const registerController = async (
 ) => {
   try {
     const result = await registerService(req.body);
+    res.status(200).send(result);
+  } catch (error) {
+    next(error);
+  }
+};
+export const loginController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const result = await loginService(req.body);
     res.status(200).send(result);
   } catch (error) {
     next(error);
