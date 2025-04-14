d48fc3392237ee9b201b9fd8b5653f516ae52c63
magicboy23 <andreangg04@gmail.com>
2025-04-14 15:48:36 +0700

    feat: validation, uploader, and emailer

--- src/routes/product.router.ts
+++ src/routes/product.router.ts
@@ -8,12 +8,22 @@ import {
 import { verifyToken } from "../lib/jwt";
 import { verifyRole } from "../middlewares/role.middleware";
 import { updateProductController } from "../controllers/product.controller";
+import { uploader } from "../lib/multer";
+import { fileFilter } from "../lib/fileFilter";
 
 const router = Router();
 
 router.get("/", getProductsController);
 router.get("/:slug", getProductBySlugController);
-router.post("/", verifyToken, verifyRole(["ADMIN"]), createProductController);
+router.post(
+  "/",
+  verifyToken,
+  verifyRole(["ADMIN"]),
+  uploader().fields([{ name: "thumbnail", maxCount: 1 }]),
+  fileFilter(["image/jpeg", "image/png", "image/avif"]),
+  createProductController
+);
+
 router.patch(
   "/:id",
   verifyToken,
--- src/services/auth/register.service.ts
+++ src/services/auth/register.service.ts
@@ -2,6 +2,10 @@ import { User } from "@prisma/client";
 import { ApiError } from "../../utils/api-error";
 import prisma from "../../config/prisma";
 import { hashPassword } from "../../lib/argon";
+import { transporter } from "../../lib/nodmailer";
+import Handlebars from "handlebars";
+import fs from "fs/promises";
+import { join } from "path";
 export const registerService = async (body: User) => {
   // cari tau dulu email nya sudah terpakai atau belom
   // kalo sudah terpakai, throw error
@@ -22,5 +26,18 @@ export const registerService = async (body: User) => {
     omit: { password: true },
   });
 
+  const templatePath = join(__dirname, "../../templates/welcome-email.hbs");
+
+  const templateSource = (await fs.readFile(templatePath)).toString();
+
+  const compiledTemplate = Handlebars.compile(templateSource);
+
+  const html = compiledTemplate({ fullName: body.fullName });
+
+  transporter.sendMail({
+    to: body.email,
+    subject: "Welcome to My App",
+    html,
+  });
   return newUser;
 };
